6cada11f44d672d7f1f0e0687ff4dec95b410ea3
souhardyaRoy <109021836+souhardyaRoy@users.noreply.github.com>
2022-09-05 14:34:10 +0530

    updateUser modified

--- app/controller/user_controller.js
+++ app/controller/user_controller.js
@@ -21,13 +21,34 @@ const createNewUser = async (request, response) => {
         return response.status(400).json(http_formatter(err, "Something went wrong, please try again", false));
     }
 }
-
 const userLogin = (request, response) => {
     
 }
 
-const updateUser = (request, response) => {
+const updateUser = async (request, response) =>  {
+    const {user_id} = request.params;
+    if(!user_id) {
+        throw (new Error('Product ID is mandatory'));
+    }
+const current_user =await User.findById(user_id);
+if(!current_user) {
+    return response.status(400).json(http_formatter({}, "Invalid user id", false));
+}
+const _body = request.body;
+['name','phone','email','password','gender'].forEach(key=>{
+    if(_body[key] !== undefined && _body[key] !== null){
+        current_user[key] = _body[key]
+    }
+})
+const address = request.body.address;
+['line1', 'state', 'pincode'].forEach(key => {     
+        if(address && address[key] !== null && address[key] !== undefined) {
+            current_user.address[key] = address[key];
+        }
+    })
 
+  const __updateduser = await current_user.save();
+  return response.status(200).json(http_formatter(__updateduser,"user updated succesfully")) 
 }
 
 const deleteUser = (request, response) => {
